a41b77e25e16f333fbc90080675d7c4599325f6e
keith <yangziyong62062@bill-jc.com>
2020-08-20 10:01:21 +0800

    Upload Day7 and Day8 code

---
+++ Day8/src/keywords/yaml_support_keywords.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Support yaml configure keywords
+1. Function config
+'''
+from robot.api.deco import keyword
+from robot.libraries.BuiltIn import BuiltIn
+from robot.api import logger
+
+
+def find_similar_key(expected_key, keys):
+    import difflib
+    result = []
+    for key in keys:
+
+        r = difflib.SequenceMatcher(None, key, expected_key).ratio()
+        if r >= 0.5:
+            result.append(key)
+    return result
+
+
+def get_all_dpaths(obj):
+    '''
+    get all response path
+    response = {
+        "a": {
+            "b":1
+        },
+        "c":2
+    }
+    return ["a", "a/b", "c"]
+    '''
+    import dpath.util
+    result = []
+    for (path, value) in dpath.util.search(obj, ['**'], yielded=True, separator="/"):
+        result.append(path)
+
+    return result
+
+
+def logger_similar_key(response, key):
+    keys = get_all_dpaths(response)
+    similar_keys = find_similar_key(key, keys)
+
+    if similar_keys:
+        print("Similar keys: {}".format(similar_keys))
+
+    else:
+        print("Could not find key: {} in the response: {}""".format(key, response))
+
+
+def foreach(obj, path):
+
+    import dpath.util
+    if not obj:
+        raise Exception("Need a list or dict parameter")
+
+    if not path:
+        raise Exception("Need a path")
+
+    b_found = False
+    for (path, value) in dpath.util.search(obj, path, yielded=True, separator="/"):
+        print(path, value)
+        b_found = True
+        return value
+
+    if not b_found:
+        logger_similar_key(obj, path)
+
+
+@keyword
+def simple_show_variable(name):
+    logger.info("the value is: {} ".format(name))
+
+
+@keyword
+def simple_function_with_param(data, index=2, flag=False):
+    logger.info("{}:{}:{}".format(data, index, flag))
+    response = ['123',
+                {
+                    'test': 'simple test',
+                    'name': 'simple name'
+                },
+                'simple msg'
+                ]
+    logger.info(response)
+    return response
+
+
+@keyword
+def run_function_keyword(step_functions, kw_name, kw_args):
+    ''' 
+        run functions which also as keywords
+        set return value as robot variable
+        saved varibale type only support: global, suite, test, common varibale
+    '''
+
+    for func in step_functions:
+        name = func.get('name')
+        args = func.get('args')
+        save_var_type = func.get('type')
+        save_var_names = func.get('saved_var_names')
+        bi = BuiltIn()
+        results = bi.run_keyword(name, *args)
+        logger.info("the receive: {}".format(save_var_names))
+        logger.info("the return value: {}".format(results))
+        for index, item in enumerate(save_var_names):
+            name = item.get('name')
+            _dpath = item.get('dpath')
+            # 根据路径获取value
+            value = foreach(results, _dpath)
+            if value:
+                args = [name, value]
+                bi.run_keyword(save_var_type, *args)
+
+    bi.log_variables()
+
+    bi.run_keyword(kw_name, *kw_args)
---
+++ Day8/src/run.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2020-07-23 16:53:58
+
+
+import sys
+import os
+import robot
+from robot.api import logger
+from robot.utils.asserts import assert_not_none
+from robot.api import TestSuite
+from robot.api import ResultWriter
+from robot.conf import RobotSettings
+from robot.libraries.BuiltIn import BuiltIn
+
+from ruamel.yaml import YAML
+from ruamel.yaml.constructor import SafeConstructor
+
+def yaml_to_obj(yaml_file):
+
+    import codecs
+    yaml = YAML()
+    yaml.allow_duplicate_keys = True
+
+    try:
+        with codecs.open(yaml_file, 'rb', 'utf-8') as f:
+            datas_dict = yaml.load(f)
+            if not datas_dict:
+                raise Exception("Please check the file: {}".format(yaml_file))
+            return datas_dict
+
+    except Exception as e:
+        raise e
+
+
+def get_yaml_configures(yaml_obj):
+    if not yaml_obj:
+        raise Exception("get_yaml_configures parameter cannot be empty")
+
+    return yaml_obj.get('config')
+
+
+def get_yaml_cases(yaml_obj):
+    if not yaml_obj:
+        raise Exception("get_yaml_cases parameter cannot be empty")
+
+    return yaml_obj.get('testcases')
+
+
+def get_all_py_files(path):
+    if not path or not os.path.exists(path):
+        raise Exception("Cannot find a path or file: {}".format(path))
+    results = []
+    for root, dirs, files in os.walk(path):
+        if not files:
+            raise Exception(
+                "Cannot find any keywords files in the path: {}".format(path))
+        for file in files:
+            if file.endswith('.py'):
+                results.append(path + "/" + file)
+
+    return results
+
+
+def create_test_keyword(suite_test, name, args=None, kw_type=None):
+
+    if not args:
+        suite_test.keywords.create(name, type=kw_type)
+    else:
+        suite_test.keywords.create(name, args=args, type=kw_type)
+
+
+def run_function(suite_test, step_functions, kw_name, kw_args, kw_type):
+    ''' 
+        run functions which also as keywords
+        set return value as robot variable
+        saved varibale type only support: global, suite, test, common varibale
+    '''
+    print(step_functions)
+    if step_functions:
+
+        suite_test.keywords.create(
+            'run_function_keyword', args=[step_functions, kw_name, kw_args], type=kw_type)
+
+
+def create_step(suite_test, obj):
+
+    for step in obj:
+        kw_name = step.get('keyword')
+        kw_args = step.get('args')
+        kw_type = step.get('type')
+        b_func = step.get('run_func')
+        if b_func:
+            run_function(suite_test, b_func, kw_name, kw_args, kw_type)
+        else:
+
+            create_test_keyword(suite_test, kw_name, kw_args, kw_type)
+
+
+def create_case_step(suite_test, testcases):
+    if not testcases:
+        logger.warn("If config test-steps into yaml file will be better")
+        return
+
+    create_step(suite_test, testcases)
+
+
+def create_assertion_step(suite_test, assertions):
+    if not assertions:
+        logger.warn("If config assertions into yaml file will be better")
+        return
+    create_step(suite_test, assertions)
+
+
+def get_setup_or_teardown_numbers(suite_steps, kw_type='setup'):
+
+    numbers = [index for index, item in enumerate(
+        suite_steps) if item.get('type') == kw_type]
+    if len(numbers) >= 2:
+        raise Exception(
+            "At least two {} were found in config steps: {}".format(kw_type, suite_steps))
+
+
+def order_suite_setup_and_teardown(suite_steps):
+    '''
+    If find teardown and setup in steps,
+    then make sure create setup keyword first
+    '''
+    get_setup_or_teardown_numbers(suite_steps)
+    get_setup_or_teardown_numbers(suite_steps, 'teardown')
+#
+    b_teardown = False
+    index_td = 0
+    import copy
+    cp_steps = copy.deepcopy(suite_steps)
+    for index, step in enumerate(suite_steps):
+
+        kw_type = step.get('type')
+        if kw_type == 'teardown':
+            b_teardown = True
+            index_td = index
+        if kw_type == 'setup':
+            if b_teardown and index > index_td:
+                cp_steps[index_td], cp_steps[index] = cp_steps[index], cp_steps[index_td]
+
+    return cp_steps
+
+
+def import_libs_by_path(suite, libs_path):
+    if not libs_path:
+        return
+
+    for path in libs_path:
+
+        libs = get_all_py_files(path)
+        for kw_file in libs:
+            suite.resource.imports.library('{}'.format(kw_file))
+
+
+yaml_file = '..\\cases.yaml'
+
+yaml_obj = yaml_to_obj(yaml_file)
+configs = get_yaml_configures(yaml_obj)
+testcases = get_yaml_cases(yaml_obj)
+
+suite_name = configs.get('suite_name')
+librarys = configs.get('librarys')
+librarys_path = configs.get('librarys_path')
+suite_steps = configs.get('steps')
+setup_numbers = [1 for item in suite_steps if item.get('type') == 'setup']
+teardown_numbers = [
+    1 for item in suite_steps if item.get('type') == 'teardown']
+
+suite = TestSuite(suite_name)
+# suite.resource.imports.library('./mykeywords.py')
+for _lib in librarys:
+    # print(_lib)
+    suite.resource.imports.library('{}'.format(_lib))
+import_libs_by_path(suite, librarys_path)
+
+# un-comment follows line, see what happen
+print(111, suite_steps)
+suite_steps = order_suite_setup_and_teardown(suite_steps)
+print(222, suite_steps)
+create_case_step(suite, suite_steps)
+
+print(suite.keywords)
+for case in testcases:
+    test_case_name = case.get('name')
+    tags = case.get('tags')
+    steps = case.get('steps')
+    assertions = case.get('assertions')
+
+    # test = suite.tests.create(test_case_name, tags=tags)
+    test = suite.tests.create(test_case_name, tags=tags)
+    # test.keywords.create('test_builtin_keyword')
+    create_case_step(test, steps)
+    create_assertion_step(test, assertions)
+
+path = "reports"
+apiname = 'skynet'
+options = {
+    "output": "{}-output.xml".format(apiname),
+    "log": "{}-log.html".format(apiname),
+    "report": "{}-reporter.html".format(apiname),
+    "outputdir": path,
+    # "include": ['CI']
+    # "exclude": ['SMOKE']
+}
+settings = RobotSettings(options)
+suite.configure(**settings.suite_config)
+result = suite.run(settings, critical='smoke')
+
+ResultWriter(settings.output if settings.log
+             else result).write_results(
+    report=settings.report, log=settings.log)
